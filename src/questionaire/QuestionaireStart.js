2771a88d83547f6c2762297e593ae66bdc60a7c2
kipronorugut <kipronorugut@gmail.com>
2024-05-13 11:56:11 +0300

    Changed title

--- src/questionaire/QuestionaireStart.js
+++ src/questionaire/QuestionaireStart.js
@@ -8,7 +8,8 @@ const QuestionaireStart = () => {
         <div className="container">
           <div className="row">
             <div className="col-lg-5 col-md-12 col-sm-12 align-self-center">
-              <h1>Begin your Organization's Self Assessment Now.</h1>
+              <h1>Welcome to A&K e-Triage</h1>
+              <h2>Begin your Organization's Self Assessment Now.</h2>
               <p>It will take about 2 minutes to complete.</p>
               
               <div className="email-box">
